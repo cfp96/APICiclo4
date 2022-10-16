50c3c363600342c93404fa720979ef855ec7e77c
cfp96 <carfelcoper@gmail.com>
2022-10-15 21:51:07 -0500

    primer commit

---
+++ src/models/servicio.model.ts
@@ -0,0 +1,42 @@
+import {Entity, model, property, belongsTo} from '@loopback/repository';
+import {Cliente} from './cliente.model';
+import {Encomienda} from './encomienda.model';
+
+@model()
+export class Servicio extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fecha: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  valor: string;
+
+  @belongsTo(() => Cliente, {name: 'origenFk'})
+  origen: string;
+
+  @belongsTo(() => Cliente, {name: 'destinoFk'})
+  destino: string;
+
+  @belongsTo(() => Encomienda, {name: 'encomiendaFk'})
+  encomienda: string;
+
+  constructor(data?: Partial<Servicio>) {
+    super(data);
+  }
+}
+
+export interface ServicioRelations {
+  // describe navigational properties here
+}
+
+export type ServicioWithRelations = Servicio & ServicioRelations;
